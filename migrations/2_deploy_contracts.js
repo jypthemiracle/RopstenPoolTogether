15c18763d55954d7f3b6412e115d59a54680d34e
Jin Hyung Park <hophfg@yahoo.co.kr>
2022-02-12 19:41:36 +0900

    config: connect to Ropsten

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,19 @@
-const SimpleStorage = artifacts.require("SimpleStorage");
-const TutorialToken = artifacts.require("TutorialToken");
-const ComplexStorage = artifacts.require("ComplexStorage");
+const basePool = artifacts.require("BasePool");
+const sortitionSumTreeFactory = artifacts.require("SortitionSumTreeFactory");
+const drawLib = artifacts.require("DrawLib");
+const constants = require("../app/src/utils/constants");
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
-  deployer.deploy(TutorialToken);
-  deployer.deploy(ComplexStorage);
+module.exports = function(deployer, network) {
+   
+    deployer.deploy(sortitionSumTreeFactory);
+    deployer.link(sortitionSumTreeFactory, [drawLib, basePool]);
+    
+    deployer.deploy(basePool, constants.cDAI_CONTRACT);
+    
+    // if (network === "kovan") {
+    //     deployer.deploy(basePool, constants.cDAI_CONTRACT_KOVAN);
+    // } else if (network === "rinkeby") {
+    //     deployer.deploy(basePool, constants.cDAI_CONTRACT_RINKEBY);
+    // }
+    
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,4 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 const path = require("path");
 
 module.exports = {
@@ -5,10 +6,28 @@ module.exports = {
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "app/src/contracts"),
   networks: {
-    develop: { // default with truffle unbox is 7545, but we can use develop to test changes, ex. truffle migrate --network develop
+    fork: { // default with truffle unbox is 7545, but we can use develop to test changes, ex. truffle migrate --network develop
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*"
+      network_id: "*",
+      // gasLimit: "0x8000000",
+      // gas: 0xfffffffff
+    },
+    ropsten:{
+      provider: () => new HDWalletProvider({
+        mnemonic: {
+          phrase: process.env.MNENOMIC
+        },
+        providerOrUrl: "https://ethereum-ropsten-rpc.allthatnode.com/" + process.env.DSRV_API_KEY
+      }),
+      network_id: 3,
+      gas: 3000000,
+      gasPrice: 10000000000
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.5.16"
     }
   }
 };
